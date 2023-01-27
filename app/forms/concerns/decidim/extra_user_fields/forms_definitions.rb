dd463c71b5437d7e2bb85a785fdb72d315728a66
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-01-27 10:56:18 +0100

    feat: new field Profession

---
+++ app/forms/concerns/decidim/extra_user_fields/forms_definitions.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module Decidim
+  module ExtraUserFields
+    # Extra user fields definitions for forms
+    module FormsDefinitions
+      extend ActiveSupport::Concern
+
+      included do
+        include ::Decidim::ExtraUserFields::ApplicationHelper
+
+        attribute :country, String
+        attribute :postal_code, String
+        attribute :date_of_birth, Decidim::Attributes::LocalizedDate
+        attribute :gender, String
+        attribute :profession, String
+
+        # Block ExtraUserFields Attributes
+
+        # EndBlock
+
+        validates :country, presence: true, if: :country?
+        validates :postal_code, presence: true, if: :postal_code?
+        validates :date_of_birth, presence: true, if: :date_of_birth?
+        validates :gender, presence: true, inclusion: { in: Decidim::ExtraUserFields::Engine::DEFAULT_GENDER_OPTIONS.map(&:to_s) }, if: :gender?
+        validates :profession, presence: true, if: :profession?
+
+
+        # Block ExtraUserFields Validations
+
+        # EndBlock
+      end
+
+      def map_model(model)
+        extended_data = model.extended_data.with_indifferent_access
+
+        self.country = extended_data[:country]
+        self.postal_code = extended_data[:postal_code]
+        self.date_of_birth = Date.parse(extended_data[:date_of_birth]) if extended_data[:date_of_birth].present?
+        self.gender = extended_data[:gender]
+        self.profession = extended_data[:profession]
+
+        # Block ExtraUserFields MapModel
+
+        # EndBlock
+      end
+
+      private
+
+      def country?
+        extra_user_fields_enabled && current_organization.activated_extra_field?(:country)
+      end
+
+      def date_of_birth?
+        extra_user_fields_enabled && current_organization.activated_extra_field?(:date_of_birth)
+      end
+
+      def gender?
+        extra_user_fields_enabled && current_organization.activated_extra_field?(:gender)
+      end
+
+      def postal_code?
+        extra_user_fields_enabled && current_organization.activated_extra_field?(:postal_code)
+      end
+
+      def profession?
+        extra_user_fields_enabled && current_organization.activated_extra_field?(:profession?)
+      end
+
+      # Block ExtraUserFields EnableFieldMethod
+
+      # EndBlock
+
+      def extra_user_fields_enabled
+        @extra_user_fields_enabled ||= current_organization.extra_user_fields_enabled?
+      end
+    end
+  end
+end
--- spec/lib/overrides_spec.rb
+++ spec/lib/overrides_spec.rb
@@ -12,7 +12,19 @@ checksums = [
       # layouts
       "/app/views/layouts/decidim/_head_extra.html.erb" => "1b8237357754cf519f4e418135f78440",
       "/app/cells/decidim/content_blocks/footer_sub_hero/show.erb" => "63cf57bc81b6e08523f3c31e7db7b1f4",
-      "/app/cells/decidim/content_blocks/sub_hero/show.erb" => "528852634af98609d2069d0f126d04a2"
+      "/app/cells/decidim/content_blocks/sub_hero/show.erb" => "528852634af98609d2069d0f126d04a2",
+    },
+  },
+  {
+    package: "decidim-extra_user_fields",
+    files: {
+      "/app/views/decidim/extra_user_fields/_profile_form.html.erb" => "d840e39f3d667b2dedddd165bfeec96e",
+      "/app/views/decidim/extra_user_fields/_registration_form.html.erb" => "b36114438b39aeee66f945cb0fcd9cc1",
+      "/app/views/decidim/extra_user_fields/admin/extra_user_fields/_form.html.erb" => "68aa08217eecd1117fad6942ab0a3703",
+      "/app/forms/concerns/decidim/extra_user_fields/forms_definitions.rb" => "67532a39998eb0cac7f313eec915a2b1",
+      "/app/forms/decidim/extra_user_fields/admin/extra_user_fields_form.rb" => "308415ba86b7753260418a533ffdd3df",
+      "/app/commands/concerns/decidim/extra_user_fields/commands_overrides.rb" => "a849a1cf505801d57ca39df9c11ccbad",
+      "/app/commands/decidim/extra_user_fields/admin/update_extra_user_fields.rb" => "bc6cd4528e845bcb029e5f811fcbc6ec",
     }
   }
 ]
