d5e2b07979985deeca98aad6185d72cce793c67e
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-05-22 14:56:23 +0200

    fix: test refactor

--- spec/system/posts_with_iframe_spec.rb
+++ spec/system/posts_with_iframe_spec.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-body = { en: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><iframe class=\"ql-video\" frameborder=\"0\" allowfullscreen=\"true\" src=\"https://youtu.be/dQw4w9WgXcQ\" data-accessibility-violation=\"true\"></iframe>", it: "" }
-
 require "rails_helper"
 
-describe "Post with Iframe", type: :system do
-  let(:post) { create :post, body: body }
-  let(:component) { Decidim::Component.find(post.decidim_component_id) }
-  let(:organization) { component.participatory_space.organization }
-  let(:slug) { component.participatory_space.slug }
+describe "post with iframe", type: :system do
+  include_context "with a component"
+  let(:manifest_name) { "blogs" }
+  let(:body) { { en: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><iframe class=\"ql-video\" frameborder=\"0\" allowfullscreen=\"true\" src=\"https://youtu.be/dQw4w9WgXcQ\" data-accessibility-violation=\"true\"></iframe>" } }
+  let(:title) { { en: "Blog post title" } }
+  let(:post) { create :post, component: component, body: body, title: title }
 
   before do
-    switch_to_host(organization.host)
-    visit decidim.root_path
+    visit_component
     page.execute_script("$('#dc-modal-accept').click()")
-    visit "/processes/#{slug}/f/#{component.id}/posts/#{post.id}"
+    visit Decidim::ResourceLocatorPresenter.new(post).path
   end
 
   it "shows the iframe" do
