6fc17bb0238ae283451b4390dd6b096ca8623e9f
Harrison Hassig <harrisonhassig@gmail.com>
2024-09-17 18:45:31 +0200

    Bump to 0.27.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_17_163754) do
+ActiveRecord::Schema.define(version: 2024_09_17_164358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
